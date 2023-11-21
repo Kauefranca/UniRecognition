5b5d106a94dc2cf7e2de54f2ac70d9a91554441c
Alexandre <belluci@gmail.com>
2023-11-21 20:55:23 -0300

    TK2

--- utils/GUI.py
+++ utils/GUI.py
@@ -4,14 +4,14 @@ from tkinter import ttk
 class SeuApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Layout Tkinter com Banco de Dados")
+        self.root.title("FaceRecognition")
 
         # Frames
         self.frame_esquerda = ttk.Frame(root, padding="10")
         self.frame_esquerda.grid(row=0, column=0, sticky="nsew")
 
         self.frame_direita = ttk.Frame(root, padding="10")
-        self.frame_direita.grid(row=0, column=1, sticky="nsew")
+        self.frame_direita.grid(row=0, column=3, sticky="nsew")
 
         # Configuração das colunas e linhas para expansão
         root.columnconfigure(0, weight=1)
@@ -23,17 +23,21 @@ class SeuApp:
 
     def criar_layout_esquerda(self):
         # Quadrado superior à esquerda
-        quadrado_superior = ttk.Label(self.frame_esquerda, text="Quadrado Superior", background="lightblue")
-        quadrado_superior.grid(row=0, column=0, pady=10)
+        quadrado_superior = ttk.Label(self.frame_esquerda, text="Video error", width=100, borderwidth=3, background="black")
+        quadrado_superior.grid(row=0, column=0, pady=10, ipady=175)
 
-        # Quadrado inferior à esquerda
-        quadrado_inferior = ttk.Label(self.frame_esquerda, text="Quadrado Inferior", background="lightgreen")
-        quadrado_inferior.grid(row=1, column=0, pady=10)
+        # Quadrado centro à esquerda
+        quadrado_centro = ttk.Button(self.frame_esquerda, text="Register", width=50)
+        quadrado_centro.grid(row=1, column=0, pady=10, ipady=10)
+
+        # Quadrado centro à esquerda
+        quadrado_centro = ttk.Button(self.frame_esquerda, text="Training", width=50)
+        quadrado_centro.grid(row=2, column=0, pady=1, ipady=10)
 
     def criar_layout_direita(self):
         # Espaço para exibir informações do banco de dados
-        info_banco_dados = ttk.Label(self.frame_direita, text="Informações do Banco de Dados", font=("Helvetica", 14))
-        info_banco_dados.grid(row=0, column=0, pady=10)
+        info_banco_dados = ttk.Label(self.frame_direita, relief="solid", text="Informações do Banco de Dados", font=("Arial", 14))
+        info_banco_dados.grid(row=0, column=0, pady=10, ipady=300 )
 
         # Adicione aqui widgets ou outros elementos para exibir informações do banco de dados
 
@@ -41,5 +45,7 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("UniRecognition")
     root.geometry('920x660')
+    root.minsize(920, 660) 
+    root.maxsize(920, 660) 
     app = SeuApp(root)
     root.mainloop()
