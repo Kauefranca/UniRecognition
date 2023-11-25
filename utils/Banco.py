0a7f0381bf721e6bc299b3f2f34fad9f2609ecf4
Kauê França <kauefranca04@gmail.com>
2023-11-25 03:12:26 -0300

    Novas funções do banco

--- utils/Banco.py
+++ utils/Banco.py
@@ -8,32 +8,49 @@ con = psycopg2.connect(
 	database='postgres', 
 	port='5432',
 	user='postgres', 
-	password='unimar')
+	password='unimar'
+)
 
 cur = con.cursor()
 
 def selectNameWithRA(ra):
-	sql = f'SELECT * FROM usuario WHERE ra={ra};'
-	cur.execute(sql)
+    sql = """SELECT * FROM usuario WHERE ra=%s;"""
+    try:
+        cur.execute(sql, (ra,))
+        return cur.fetchall()
+    except Exception as e:
+        print(f"Error: {e}")
 
-	recset = cur.fetchall()
-	print(recset)
-	# return recset[0][0]
-
-
-def createUser(name, ra):
-	sql = f"""INSERT INTO usuario(nome, ra) VALUES('{name}', {ra});"""
-	cur.execute(sql)
-	con.commit()
-
+def createUser(nome, ra):
+    sql = """INSERT INTO usuario(nome, ra) VALUES(%s, %s);"""
+    try:
+        cur.execute(sql, (nome, ra))
+        con.commit()
+    except Exception as e:
+        print(f"Error: {e}")
+     
+def createProfessor(nome):
+    sql = """INSERT INTO professor(nome) VALUES(%s);"""
+    try:
+        cur.execute(sql, (nome,))
+        con.commit()
+    except Exception as e:
+        print(f"Error: {e}")
+     
+def createAula(nome, id_professor):
+    sql = """INSERT INTO aula(nome, id_professor) VALUES(%s, %s);"""
+    try:
+        cur.execute(sql, (nome, id_professor))
+        con.commit()
+    except Exception as e:
+        print(f"Error: {e}")
 
 def insertImage(image_path, ra):
-    # Read the image file as binary data
+    # Abrir imagem como binário
     with open(image_path, 'rb') as file:
         image_binary = file.read()
 
-    # Insert the image binary data into the database
-    sql = 'INSERT INTO imagem(imagem, id_usuario) VALUES (%s, (SELECT id_usuario FROM usuario WHERE ra = %s));'
+    sql = """INSERT INTO imagem(imagem, id_usuario) VALUES (%s, (SELECT id_usuario FROM usuario WHERE ra = %s));"""
     try:
         cur.execute(sql, (image_binary, ra))
         con.commit()
@@ -42,20 +59,16 @@ def insertImage(image_path, ra):
         print(f"Error: {e}")
         
 def selectImage(ra):
-    # Execute a query to select the image data
-    sql = 'SELECT imagem FROM imagem WHERE id_usuario = (SELECT id_usuario FROM usuario WHERE ra = %s);'
+    sql = """SELECT imagem FROM imagem WHERE id_usuario = (SELECT id_usuario FROM usuario WHERE ra = %s);"""
     try:
         cur.execute(sql, (ra,))
         result = cur.fetchone()
 
         if result:
-            # Retrieve image data from the result
             image_data = result[0]
 
-            # Create an image object from the binary data
             image = Image.open(BytesIO(image_data))
 
-            # Display or process the image as needed
             image.show()
 
         else:
@@ -63,13 +76,35 @@ def selectImage(ra):
     except Exception as e:
         print(f"Error: {e}")
 
-selectImage(1959642)
+def selectAllImages():
+    sql = """SELECT * FROM imagem;"""
+    try:
+        cur.execute(sql)
+        result = cur.fetchall()
+
+        if result:
+            return result
+
+        else:
+            print("Image not found for the given ra.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+# selectImage(1959642)
      
 # createUser("Eduardo Santos", 1959642)
 
 # for path in listdir('./fotos/1959642/'):
 # 	insertImage('./fotos/1959642/' + path, 1959642)
      
-# selectNameWithRA('1959642')
-con.close()
+# name = selectNameWithRA('1959642')
+# print(name)
 
+# createProfessor("Rafael Gutierres")
+
+# createAula('Fábrica de projeto ágeis', 1)
+images = selectAllImages()
+
+print(images)
+
+con.close()
